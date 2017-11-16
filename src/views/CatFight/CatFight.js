76932ef3c88b99fc88811a02c5948d9197d8bbf9
connerj70 <connerj70@outlook.com>
2017-11-16 10:53:26 -0700

    styling catfight page

--- src/views/CatFight/CatFight.js
+++ src/views/CatFight/CatFight.js
@@ -55,13 +55,21 @@ export default class CatFight extends Component {
                 <div className="catfight_wrapper">
                     <Output results={this.state.testResults}/>
                     <div className='catfight_editor'>
+                        <div className="catfight_editor-header">Solution: <i className="fa fa-arrows-alt" aria-hidden="true"></i></div>
                         <Editor fight={this.state.fight} click={this.state.click} onChange={this.onChange} code={this.state.code}/>
                         <div className="catfight_tests">
                             <div className='catfight_tests-div'>
-                                <div className="catfight_tests-header"></div>
+                                <div className="catfight_tests-header"><div>Sample Tests:</div><div><i className="fa fa-arrows-alt" aria-hidden="true"></i><i className="fa fa-question-circle" aria-hidden="true"></i></div></div>
                             </div>
                             <div className="catfight_button-container">
-                                <button className='catfight_button'onClick={() => this.handleClick()}>Submit</button>
+                                <div>
+                                    <button className='catfight_skip-button'onClick={() => this.handleClick()}><i className="fa fa-forward" aria-hidden="true"></i>SKIP</button>
+                                </div>
+                                <div>
+                                    <button className='catfight_reset-button'onClick={() => this.handleClick()}>RESET</button>
+                                    <button className='catfight_sample-button'onClick={() => this.handleClick()}>RUN SAMPLE TESTS</button>
+                                    <button className='catfight_attempt-button'onClick={() => this.handleClick()}><i className="fa fa-caret-right" aria-hidden="true"></i>ATTEMPT</button>
+                                </div>
                             </div>
                         </div>
                     </div>
--- src/views/CatFight/Output/Output.js
+++ src/views/CatFight/Output/Output.js
@@ -3,6 +3,10 @@ import React, {Component} from 'react';
 class Output extends Component {
     constructor() {
         super()
+
+        this.state = {
+            active: true
+        }
     }
 
     componentWillReceiveProps(nextProps) {
@@ -11,10 +15,19 @@ class Output extends Component {
         }
     }
 
+    handleClick() {
+        this.setState({
+            active: !this.state.active
+        })
+    }
+
     render() {
         return (
             <div className="catfight_output">
-                <div className="catfight_output-header"></div>
+                <div className="catfight_output-header">
+                        <div className={this.state.active ? "catfight_output-active" : "catfight_output-plain"} onClick={() => this.handleClick()}>Instructions</div>
+                        <div className={this.state.active ? "catfight_output-plain" : 'catfight_output-active'} onClick={() => this.handleClick()}>Output</div>
+                </div>
             </div>
         )
     }
