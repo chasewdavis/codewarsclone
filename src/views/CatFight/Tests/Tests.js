0390e8de70b9084fa87f9c3fb48d8c3a54b81033
Thomas Lowry <minilao94@yahoo.com>
2017-11-17 13:37:43 -0700

    working on styles

--- src/views/CatFight/Tests/Tests.js
+++ src/views/CatFight/Tests/Tests.js
@@ -1,51 +1,62 @@
 import React, { Component } from 'react';
 import './Tests.css';
 
-function Test(props) {
+class Test extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            open: false
+        }
+    }
     // console.log(props.parameters)
     // console.log(props.types)
-    return (
-        <div className='Test'>
-            TEST # {props.id + 1}
-            <div className='parameter-box'>
-                Parameters: (separate parameters by comma)
+    render() {
+        let props = this.props
+        return (
+            <div className='Test'>
+                TEST # {props.id + 1}
+                <div className='open-test'>
+                    <div className='parameter-box'>
+                        Parameters: (separate parameters by comma)
             {
-                    props.parameters.length ?
-                        props.parameters.map((param, i) => {
-                            return (
-                                <div className='input-line' >
-                                    <div>Parameter {i + 1}:</div>
-                                    <input key={i} onChange={e => props.change(props.id, 'params', e.target.value, i)} value={param} />
-                                </div>
-                            )
-                        })
-                        :
-                        <div className='test-input-placeholder' />
-                }
-            </div>
-            <div className='types-box'>
-                Parameter Types: (separate parameters by comma)
+                            props.parameters.length ?
+                                props.parameters.map((param, i) => {
+                                    return (
+                                        <div className='input-line' >
+                                            <div>Parameter {i + 1}:</div>
+                                            <input key={i} onChange={e => props.change(props.id, 'params', e.target.value, i)} value={param} />
+                                        </div>
+                                    )
+                                })
+                                :
+                                <div className='test-input-placeholder' />
+                        }
+                    </div>
+                    <div className='types-box'>
+                        Parameter Types: (separate parameters by comma)
                 {
-                    props.types.length ?
-                        props.types.map((type, i) => {
-                            return (
-                                <div className='input-line' >
-                                    <div>Type of Parameter {i + 1}:</div>
-                                    <input key={i} onChange={e => props.change(props.id, 'types', e.target.value, i)} value={type} />
-                                </div>
-                            )
-                        })
-                        :
-                        <div className='test-input-placeholder' />
-                }
-            </div>
-            <div className='expected-result-box'>
-                <div className='input-line'>
-                    Expected Result: <input onChange={e => props.change(props.id, 'result', e.target.value)} value={props.result} />
+                            props.types.length ?
+                                props.types.map((type, i) => {
+                                    return (
+                                        <div className='input-line' >
+                                            <div>Type of Parameter {i + 1}:</div>
+                                            <input key={i} onChange={e => props.change(props.id, 'types', e.target.value, i)} value={type} />
+                                        </div>
+                                    )
+                                })
+                                :
+                                <div className='test-input-placeholder' />
+                        }
+                    </div>
+                    <div className='expected-result-box'>
+                        <div className='input-line'>
+                            Expected Result: <input onChange={e => props.change(props.id, 'result', e.target.value)} value={props.result} />
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default class Tests extends Component {
