099cee1d29554e48ce2373282dc44e830a8d67f6
Chase Davis <chasewdavis7@gmail.com>
2017-11-27 14:24:16 -0700

    Search View and DataBase

--- src/views/Search/SearchResults/SearchResults.js
+++ src/views/Search/SearchResults/SearchResults.js
@@ -1,21 +1,53 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Diff } from '../../../components/Buttons/Buttons';
+import './SearchResults.css';
 
 class SearchResults extends Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            results: []
         }
+    }
+
+    styleTags(arr){
+        console.log(arr)
+        return arr.map((tag,i) => {
+            console.log(tag);
+            return (
+                <div key={i}>
+                    {tag.toUpperCase()}
+                </div>
+            )
+        })
     }
     
     render() {
 
         console.log('from search results...', this.props.searchResults)
 
+        let results = this.props.searchResults.map(challenge => {
+            return (
+                <div>
+                    <div>
+                        <Diff isButton={false} rating={challenge.difficulty}/>
+                        {challenge.name}
+                    </div>
+                    <div>
+                        {challenge.username}
+                    </div>
+                    <div className='searchResults_tags'>
+                        {this.styleTags(challenge.tags)}
+                    </div>
+                    <div className='bottom-border'></div>
+                </div>
+            )
+        })
+
         return (
             <div className='searchResults'>
-                
+                {results}
             </div>
         )
     }
